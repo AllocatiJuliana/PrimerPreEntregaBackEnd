b1cca496de4a51d955aab901b2e3a7b5515adbc0
Juliana Allocati <juliana_allocati@hotmail.com>
2023-07-20 21:39:48 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 import express from "express";
-import { productsRouter } from "./routes/products.routes.js";
-import { cartRouter } from "./routes/carts.routes.js";
+import handlebars from "express-handlebars";
+import { Server as SocketServer } from "socket.io";
+import __dirname from "./config/dirname.js";
+
+// import { productsRouter } from "./routes/products.routes.js";
+// import { cartRouter } from "./routes/carts.routes.js";
 
 const app = express();
-app.use(express.json());
 
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartRouter);
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
 
-app.listen(8080, () => {
+// app.use(express.json());
+// app.use("/api/products", productsRouter);
+// app.use("/api/carts", cartRouter);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const appServer = app.listen(8080, () => {
   console.log("server runnung 8080");
+});
+
+const socketServer = new SocketServer(appServer);
+
+socketServer.on("connection", (socket) => {
+  console.log(socket);
 });
